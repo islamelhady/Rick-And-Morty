435d21330480c1962488218ab5233f8d5f4f4dbf
islamelhady <i.elhady44@gmail.com>
2021-03-07 21:35:26 +0200

    Add database

--- app/src/main/java/com/elhady/rickandmorty/data/entities/Character.kt
+++ app/src/main/java/com/elhady/rickandmorty/data/entities/Character.kt
@@ -1,10 +1,15 @@
 package com.elhady.rickandmorty.data.entities
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 
 /**
  * Created by islam elhady on 05-Mar-21.
  */
+@Entity(tableName = "characters")
 data class Character(
+    @PrimaryKey
     val id: Int,
     val created: String,
     val gender: String,
