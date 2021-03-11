76a01bbab0c81a088b11098bb5a3a35542f7f9f1
islamelhady <i.elhady44@gmail.com>
2021-03-11 22:15:01 +0200

    edit character dao

--- app/src/main/java/com/elhady/rickandmorty/data/local/CharacterDao.kt
+++ app/src/main/java/com/elhady/rickandmorty/data/local/CharacterDao.kt
@@ -10,17 +10,10 @@ import com.elhady.rickandmorty.data.entities.Characters
 @Dao
 interface CharacterDao {
 
-    @Query("SELECT * FROM characters")
-    fun getAllCharacters() : LiveData<List<Characters>>
-
-//    @Query("SELECT * FROM characters WHERE id = :id")
-//    fun getCharacter(id: Int): LiveData<Characters>
-
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAll(characters: List<Characters>)
 
-//    @Insert(onConflict = OnConflictStrategy.REPLACE)
-//    suspend fun insert(character: Characters)
-
+    @Query("SELECT * FROM characters")
+    fun getAllCharacters() : LiveData<List<Characters>>
 
 }
